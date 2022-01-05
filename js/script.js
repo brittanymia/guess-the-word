e1199056c16b5068733fc83f6a58d42c510d93cd
brittany_mia <brittany.harden15@gmail.com>
2022-01-05 14:50:18 -0600

    Accept & validate player guesses

--- js/script.js
+++ js/script.js
@@ -8,21 +8,51 @@ const message = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
 
 const word = "magnolia";
+const guessedLetters = [];
 
-const placeholder = function () {
+const placeholder = function (word) {
     const placeholderLetters = [];
     for (const letter of word) {
         console.log(letter);
         placeholderLetters.push("●");
-      }
-      wordInProgress.innerText = placeholderLetters.join("");
+    }
+    wordInProgress.innerText = placeholderLetters.join("");
 };
     
-    placeholder(word);
+placeholder(word);
     
-    guessLetterButton.addEventListener("click", function (e) {
-      e.preventDefault();
-      const guess = letterInput.value;
-      console.log(guess);
-      letterInput.value = "";
+guessLetterButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    message.innerText = "";
+    const guess = letterInput.value;
+    const goodGuess = validateInput(guess);
+
+    if (goodGuess){
+        makeGuess(guess);
+    }
+    letterInput.value = "";
 });
+
+const validateInput = function (input) {
+    const acceptedLetter = /[a-zA-Z]/;
+    if (input.length === 0) {
+       message.innerText = "Enter a letter."; 
+    } else if (input.length > 1) {
+        message.innerText = "Just one single letter my friend.";
+    } else if (!input.match(acceptedLetter)) {
+        message.innerText = "Enter a letter not a number dude.";
+    } else {
+        return input;
+    }
+};
+
+const makeGuess = function (guess) {
+    guess = guess.toUpperCase();
+    if (guessedLetters.includes(guess)) {
+        message.innerText = "You already guessed that letter, hun. Try again.";
+    } else { 
+        guessedLetters.push(guess);
+        console.log(guessedLetters);
+    }
+};
+
